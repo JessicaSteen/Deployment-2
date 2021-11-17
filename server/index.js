b2d8404857b42db96eff053a0141619d864629b7
Jessica Steen <jessicalsteen@yahoo.com>
2021-11-17 15:01:42 -0700

    getting css to work

--- server/index.js
+++ server/index.js
@@ -7,8 +7,8 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../index.html'))
 });
 
-app.get('/js', (req, res) => {
-    res.sendFile(path.join(__dirname, '..index.html'))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..index.css'))
   })
 
 const port = process.env.PORT || 4005
