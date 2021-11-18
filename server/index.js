734a04dec8d003e1ebc71a1a3cd929386b166886
Jessica Steen <jessicalsteen@yahoo.com>
2021-11-18 14:24:06 -0700

    html changes

--- server/index.js
+++ server/index.js
@@ -7,11 +7,9 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../public/index.html'))
 });
 
-// app.get('/css', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../public/index.css'))
-// });
-
-app.use('/css', express.static(path.join(__dirname, '../public/index.css'));
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.css'))
+});
 
 const port = process.env.PORT || 4005
 
